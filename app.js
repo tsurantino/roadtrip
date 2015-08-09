02192fc174c01093cfc5ca54f56c389fb9482ed5
Artur Tsurkan <arturtsurkan@gmail.com>
2015-08-09 16:50:16 -0400

    Removed router because it's not necessary and we'll be doing other things in app.js to work around this DB. Added proof-of-concept for reading from our text DB, need to turn that into a data structure.

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-
+var fs = require('fs');
+var lazy = require('lazy');
 var app = express();
 
 // view engine setup
@@ -21,7 +20,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+app.get('/nearby-cities', function(req, res) {
+  new lazy(fs.createReadStream(path.join(__dirname, '../', 'public/files/cities15000.txt')))
+    .lines
+    .forEach(function(line) {
+      line = line.toString().split("\t");
+
+      if (line[8] == 'CA')
+        console.log(line[8] + ': ' + line[1] + '\t\t' + line[4] + '\t' + line[5]);
+    }
+  );
+
+  res.redirect('/');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
