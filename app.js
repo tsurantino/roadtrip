707c6f6dc0335c5f46e7cc4b74cedb5a2632037a
Artur Tsurkan <arturtsurkan@gmail.com>
2015-08-10 00:33:35 -0400

    Added US. Toyed with some Yelp stuff, but that continues in another branch.

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var locTree = require('./lib/locTree');
+var yelp = require("yelp").createClient({
+  consumer_key: "B6bWEcfpjbithTKt2uuGvg", 
+  consumer_secret: "fxEsn1IZ2-G2E4q-OrXp6B0jCew",
+  token: "EEAB9_3JVCCrMAnAHexPqPoCCRpEQ7Li",
+  token_secret: "g0F3MtRIHoV8bFSXyyfO4OyBCfg"
+});
 
+var locTree = require('./lib/locTree');
 locTree(function(locTree) {
   console.log('Testing...')
   console.log(locTree.nearest({ x: 43.822014, y: -79.109414 }, 1, 10));
@@ -20,7 +26,7 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.json({limit: '500mb'}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -30,22 +36,46 @@ app.get('/', function(req, res, next) {
 });
 
 app.post('/nearby-cities', function(req, res) {
-  console.log('Received request.');
-  
-  
   locTree(function (locTree) {
+    unique_cities = {}
     req.body.paths.forEach(function(path) {
       results = locTree.nearest({ x: path.x, y: path.y }, 1, 10);
-
       if (results.length > 0) {
         city = results[0][0].city;
 
-        if (!(city in result))
-          result[city] = city;
+        if (!(city in unique_cities))
+          unique_cities[city] = city;
       }
     });
 
-    console.log(Object.keys(result));
+    Object.keys(unique_cities).forEach(function(city) {
+      console.log(city);
+      /*yelp.search({
+        category_filter: 'food,restaurants',
+        location: city, 
+        limit: 3, 
+        sort: 2, // sort mode: 2=Highest Rated
+      }, function(error, data) {
+        if (error) {
+          console.log(error);
+        }
+        else {
+          if (data['businesses'] && data['businesses'].length > 0) {
+            // we only need some of the properties of a Yelp business result
+            results = data['businesses'].map(function(business) {
+              return {
+                image_url: business.image_url,
+                name: business.name,
+                url: business.url,
+                rating_img_url: business.rating_img_url,
+                location: business.location.display_address.join(', '),
+              }
+            });
+            console.log(results);
+          }
+        }
+      });*/
+    });
     
     res.contentType('json');
     res.status(200);
